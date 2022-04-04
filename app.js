da2ddf31737a3f0c1cddb94a06d8420238c3f2d5
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-04 14:41:20 +0100

    login fixed

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session=require("express-session")
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf')
 const app = express();
 
 
@@ -14,13 +17,9 @@ const store = new MongoDBStore({
   uri: 'mongodb+srv://mongoose:john123@cluster0.xcjno.mongodb.net/myMongooseDatabase',
   collection: 'sessions',
 });
-
+const csrfProtection = csrf()
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret:"my secret", resave:false,saveUninitialized:false,store:store}))
@@ -37,7 +36,7 @@ app.use((req, res, next) => {
     })
     .catch(err => console.log(err));
 });
-
+app.use(csrfProtection)
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
